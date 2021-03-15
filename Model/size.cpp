d04499a35c209fd96382ee0e4ebe627750e8babe
Yana <70914780+yanapush@users.noreply.github.com>
2021-03-15 10:28:19 +0300

    Updated Size class

--- Model/size.cpp
+++ Model/size.cpp
@@ -1,1 +1,54 @@
 #include "size.h"
+#include "constants.h"
+
+Size::Size(double x, double y) noexcept : width_(x), height_(y) {}
+
+Size Size::operator*(double rhs) const {
+  return Size(width_ * rhs, height_ * rhs);
+}
+
+Size Size::operator/(double rhs) const {
+  return Size(width_ / rhs, height_ / rhs);
+}
+
+Size &Size::operator*=(double rhs) {
+  *this = *this * rhs;
+  return *this;
+}
+
+Size &Size::operator/=(double rhs) {
+  *this = *this / rhs;
+  return *this;
+}
+
+Size Size::operator+(Size rhs) const {
+  return Size(width_ + rhs.width_, height_ + rhs.height_);
+}
+
+Size Size::operator-(Size rhs) const {
+  return Size(width_ - rhs.width_, height_ - rhs.height_);
+}
+
+Size &Size::operator+=(Size rhs) {
+  *this = *this + rhs;
+  return *this;
+}
+
+Size &Size::operator-=(Size rhs) {
+  *this = *this - rhs;
+  return *this;
+}
+
+bool Size::operator==(Size rhs) const {
+  return (width_ - rhs.width_ < constants::kEpsilon)
+      && (height_ - rhs.height_ < constants::kEpsilon);
+}
+
+bool Size::operator!=(Size rhs) const {
+  return !(*this == rhs);
+}
+
+std::pair<double, double> Size::GetSize() {
+  return std::pair(width_, height_);
+}
+
--- Model/size.h
+++ Model/size.h
@@ -1,7 +1,33 @@
 #ifndef MODEL_SIZE_H_
 #define MODEL_SIZE_H_
 
+#include <utility>
+#include <cmath>
+
 class Size {
+ public:
+  Size() = default;
+  Size(double x, double y) noexcept;
+
+  std::pair<double, double> GetSize();
+
+  Size operator*(double rhs) const;
+  Size operator/(double rhs) const;
+  Size& operator*=(double rhs);
+  Size& operator/=(double rhs);
+
+  Size operator+(Size rhs) const;
+  Size operator-(Size rhs) const;
+  Size& operator+=(Size rhs);
+  Size& operator-=(Size rhs);
+
+  bool operator==(Size rhs) const;
+  bool operator!=(Size rhs) const;
+
+ private:
+  double width_ = 0;
+  double height_ = 0;
 };
+
 
 #endif  // MODEL_SIZE_H_
