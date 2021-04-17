4595d270cd3a5002d355569da3ee8b3cb02b3554
Yana <70914780+yanapush@users.noreply.github.com>
2021-04-17 19:38:24 +0300

    Added animation class and 2 animations for static cats.

---
+++ View/animation.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Yana on 15.04.21.
+//
+
+#include "animation.h"
+
+Animation::Animation(const std::vector<std::vector<QPixmap>> &frames, int animation_duration) : frames_(frames)
+        , frames_rescaled_(frames)
+{
+    time_between_frames_ = 1.0 * animation_duration / frames.size();
+    Reset();
+}
+
+void Animation::Tick(int delta_time, bool is_moving) {
+    wait_till_next_frame_ -= delta_time;
+    if (wait_till_next_frame_ >= 0) {
+        return;
+    }
+    if (!is_moving) {
+        std::srand(time(nullptr));
+        if (current_frame_ == 26  && animation_loops_number == 0) {
+
+ //            current_road_ = std::srand() % 8 + 5;
+                current_road_ = (std::rand() % 3) + 5;
+
+                if (current_road_ != 5) {
+                    animation_loops_number = 1;
+                } else {
+
+                    animation_loops_number = std::rand() % 3 + 1;
+                }
+        } else {
+            if (current_frame_ == 26) {
+
+                --animation_loops_number;
+            }
+        }
+    }
+    current_frame_ = (current_frame_ + 1) % frames_.at(current_road_).size();
+    wait_till_next_frame_ = time_between_frames_;
+}
+
+void Animation::Reset() {
+    wait_till_next_frame_ = time_between_frames_;
+}
+
+const QPixmap &Animation::GetCurrentFrame() const {
+    return frames_rescaled_[current_road_][current_frame_];
+
+}
+
+const std::vector<QPixmap>& Animation::GetCurrentAnimationRoad() const {
+    return frames_rescaled_[current_road_];
+}
+
+int Animation::GetCurrentAnimationDuration() const {
+    return frames_.at(current_road_).size() * time_between_frames_;
+}
+
+void Animation::Rescale(Size to_size) {
+    if (frames_.at(current_road_).empty()) {
+        return;
+    }
+    if (std::abs(picture_size_.GetWidth() - to_size.GetWidth())
+        + std::abs(picture_size_.GetHeight() - to_size.GetHeight()) < 5) {
+        return;
+    }
+    for (uint32_t i = 0; i < frames_.at(current_road_).size(); i++) {
+        frames_rescaled_[current_road_][i] =
+                (frames_)[current_road_][i].scaled(to_size.GetWidth() + 1, to_size.GetHeight() + 1,
+                                                   Qt::KeepAspectRatio);
+    }
+    picture_size_ = to_size;
+}
+
+void Animation::SetCurrentRoad(int road) {
+    current_road_ = road;
+}
+
+
---
+++ View/animation.h
@@ -0,0 +1,43 @@
+//
+// Created by Yana on 15.04.21.
+//
+
+#ifndef VIEW_ANIMATION_H
+#define VIEW_ANIMATION_H
+
+#include <iostream>
+#include <vector>
+#include <QPixmap>
+
+#include "../Model/constants.h"
+#include "../Model/size.h"
+
+
+class Animation {
+public:
+    Animation() = default;
+    explicit Animation(const std::vector<std::vector<QPixmap>>& frames,
+              int animation_duration = constants::kTimeScale);
+
+    void Tick(int delta_time, bool is_moving = false);
+    void Reset();
+    const QPixmap& GetCurrentFrame() const;
+    const std::vector<QPixmap>& GetCurrentAnimationRoad() const;
+    int GetCurrentAnimationDuration() const;
+    void Rescale(Size to_size);
+    void SetCurrentRoad(int road);
+
+private:
+    Size picture_size_{0, 0};
+
+    //--------
+    uint animation_loops_number = 2;
+    uint current_road_ = 5;
+    uint current_frame_ = 0;
+    int wait_till_next_frame_ = 0;
+    int time_between_frames_ = 0;
+
+    std::vector<std::vector<QPixmap>> frames_;
+    std::vector<std::vector<QPixmap>> frames_rescaled_;
+};
+#endif //VIEW_ANIMATION_H
