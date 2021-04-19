0a4f7bfc1e64d3e007ebb12bd95cb3b206245a65
Polina Makruseva <polimakruseva@gmail.com>
2021-04-19 15:06:49 +0300

    Code style fix

--- GameObject/moving_object.cpp
+++ GameObject/moving_object.cpp
@@ -16,8 +16,9 @@ void MovingObject::MoveToDestination(int delta_time) {
   }
   auto first_vector = (position_ + velocity_).GetVectorTo(destination_);
   auto second_vector = position_.GetVectorTo(destination_);
-  if (first_vector.GetWidth() * second_vector.GetWidth() <= constants::kEpsilon &&
-      first_vector.GetHeight() * second_vector.GetHeight() <= constants::kEpsilon) {
+  if (first_vector.GetWidth() * second_vector.GetWidth() <=
+  constants::kEpsilon && first_vector.GetHeight() * second_vector.GetHeight()
+  <= constants::kEpsilon) {
     position_ = destination_;
   } else {
     position_ += velocity_;
