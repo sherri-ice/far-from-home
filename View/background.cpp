c2252720ee35b33933de4370495028ec3c75cdcd
sherri-ice <sherri.ice7@gmail.com>
2021-05-26 01:23:50 +0300

    background image resize

--- View/background.cpp
+++ View/background.cpp
@@ -2,15 +2,23 @@
 
 #include <QDebug>
 
-void Background::Draw(QPainter* painter, Resizer* resizer, const Point& center) {
+void Background::Draw(QPainter* painter,
+                      Resizer* resizer,
+                      const Point& center) {
   painter->save();
-  painter->translate(QPoint(0, 0));
-  auto new_coordinates = resizer->GameToWindowCoordinate(center_);
-  auto new_coordinates_q = QPoint(new_coordinates.GetX(), new_coordinates.GetY());
+  painter->translate(-QPoint(center.GetX() - background_image_.width() / 2,
+                             center.GetY() - background_image_.height() / 2));
+
+  auto new_coordinates = resizer->GameToWindowCoordinate(center);
+  auto new_coordinates_q =
+      QPoint(new_coordinates.GetX() - background_image_.width() / 2,
+             new_coordinates.GetY() - background_image_.height() / 2);
   auto
-      new_size = resizer->GameToWindowSize(Size(rect_.width(), rect_.height()));
-  auto new_rect = QRect(0, 0, new_size.GetWidth(), new_size.GetLength());
-  painter->drawPixmap(new_coordinates_q,
+      new_size = resizer->GameToWindowSize(Size(background_image_.width(), background_image_.height()));
+  painter->drawPixmap(new_coordinates_q.x(),
+                      new_coordinates_q.y(),
+                      new_size.GetWidth(),
+                      new_size.GetHeight(),
                       background_image_);
   painter->restore();
 }
