12e9b516b8ac526afcc71e24342b9519026d55b9
sherri-ice <sherri.ice7@gmail.com>
2021-05-26 02:06:36 +0300

    background image resize

--- View/background.cpp
+++ View/background.cpp
@@ -6,17 +6,20 @@ void Background::Draw(QPainter* painter,
                       Resizer* resizer,
                       const Point& center) {
   painter->save();
-  painter->translate(-QPoint(center.GetX() - background_image_.width() / 2,
-                             center.GetY() - background_image_.height() / 2));
-
-  auto new_coordinates = resizer->GameToWindowCoordinate(center);
-  auto new_coordinates_q =
-      QPoint(new_coordinates.GetX() - background_image_.width() / 2,
-             new_coordinates.GetY() - background_image_.height() / 2);
   auto
-      new_size = resizer->GameToWindowSize(Size(background_image_.width(), background_image_.height()));
-  painter->drawPixmap(new_coordinates_q.x(),
-                      new_coordinates_q.y(),
+      new_size = resizer->GameToWindowSize(Size(background_image_.width(),
+                                                background_image_.height()));
+  auto new_coordinates = resizer->GameToWindowCoordinate(center);
+  painter->translate(-QPoint(
+      new_coordinates.GetX() - background_image_.width() / 2,
+      new_coordinates.GetY() - background_image_.height() / 2));
+
+  auto new_coordinates_q =
+      QPoint(new_coordinates.GetX(),
+             new_coordinates.GetY());
+
+  painter->drawPixmap(new_coordinates_q.x() - new_size.GetWidth() / 2,
+                      new_coordinates_q.y() - new_size.GetHeight() / 2,
                       new_size.GetWidth(),
                       new_size.GetHeight(),
                       background_image_);
