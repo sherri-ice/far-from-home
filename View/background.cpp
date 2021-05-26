80465ad15eac55af9ee83d886ec771360c2fcfc6
Yana <70914780+yanapush@users.noreply.github.com>
2021-05-26 10:33:24 +0300

    cs

--- View/background.cpp
+++ View/background.cpp
@@ -6,7 +6,7 @@ void Background::Draw(QPainter* painter,
                       Resizer* resizer,
                       const Point& center) {
   painter->save();
-  painter->setBrush(QBrush(QColor(124,252,0)));
+  painter->setBrush(QBrush(QColor(124, 252, 0)));
   painter->drawRect(QRect(0, 0, 7000, 7000));
   painter->restore();
 }
